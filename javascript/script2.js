a40d69617323342f1df18965ca5e0dabf2ceb66c
Gene515-m <151438586+Gene515-m@users.noreply.github.com>
2024-02-09 16:13:52 +0100

    Add files via upload

---
+++ javascript/script2.js
@@ -0,0 +1,6 @@
+console.log("Variabile = è come una scatola che può contenere qualsiasi tipo di informazioni e una volta creata questa scatol può essere riutilizzata. es. come le scatole del trasloco metti i libri e una volta che hai svuotato, puoi mettere altri tipi di oggetti");
+
+console.log("Algoritmo = è una sequenza di istruzioni per risolvere un problema, es. come arrivare da un punto A a punto B più o meno come un navigatore che ti dice come arrivare alla destinazione");
+
+
+console.log("Undefined e null sembrano la stessa ma non lo sono, null è proprio vuoto e invece undefined è sempre vuoto ma può essere assegnato un valore in un secondo momento");
